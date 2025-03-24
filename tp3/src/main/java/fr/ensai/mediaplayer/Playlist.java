81e992f0f929bfeae153ae05b54c3f73cc00bc36
id2380 <thibaut.louza@eleve.ensai.fr>
2025-03-24 11:50:53 +0000

    sixième commit

--- tp3/src/main/java/fr/ensai/mediaplayer/Playlist.java
+++ tp3/src/main/java/fr/ensai/mediaplayer/Playlist.java
@@ -7,17 +7,15 @@ package fr.ensai.mediaplayer;
 public class Playlist {
     private String name;
     private List<Media> mediaList = new ArrayList<>();
-    private int totalDuration ;
+    private int totalDuration == null;
 
     /**
      * Constructs a new Playlist object.
      * @param name  The name of the playlist.
-     * @param mediaList  The list of medias of the playlist.
      * @param totalDuration  The total duration of the playlist.
      */
-    public Playlist(String name, List<Media> mediaList, int totalDuration) {
+    public Playlist(String name, int totalDuration) {
         this.name = name ;
-        this.mediaList = mediaList ;
         this.totalDuration = totalDuration ; 
     }
     
