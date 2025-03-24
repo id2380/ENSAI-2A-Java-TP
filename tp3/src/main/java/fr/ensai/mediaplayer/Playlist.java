bb5e2fbf38064e5326442c1f5efbac3e853cbd1f
id2380 <thibaut.louza@eleve.ensai.fr>
2025-03-24 11:51:59 +0000

    septième commit

--- tp3/src/main/java/fr/ensai/mediaplayer/Playlist.java
+++ tp3/src/main/java/fr/ensai/mediaplayer/Playlist.java
@@ -12,11 +12,9 @@ public class Playlist {
     /**
      * Constructs a new Playlist object.
      * @param name  The name of the playlist.
-     * @param totalDuration  The total duration of the playlist.
      */
     public Playlist(String name, int totalDuration) {
         this.name = name ;
-        this.totalDuration = totalDuration ; 
     }
     
     // Add an media to the podcast
